cbaf8eafeccd96f766b8b0e2556bf253571ae53e
UtkarshChaurasia <msdianuc07@gmail.com>
2020-12-09 18:42:16 +0530

    setup and assets added

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:AIDIO/utils/ai_util.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: AIColors.primaryColor1,
+    );
+  }
+}

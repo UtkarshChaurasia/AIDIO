02628e821c49f94e816a2deb2287f83ae58e730c
UtkarshChaurasia <msdianuc07@gmail.com>
2020-12-10 00:03:32 +0530

    initial ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:AIDIO/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(AIDIO());
 }
 
@@ -10,7 +14,7 @@ class AIDIO extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "AIDIO",
-      theme: ThemeData(),
+      theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
       home: HomePage(),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:AIDIO/model/radio.dart';
 import 'package:AIDIO/utils/ai_util.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,10 +11,107 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<MyRadio> radios;
+
   @override
+  void initState() {
+    super.initState();
+    fetchRadio();
+  }
+
+  fetchRadio() async {
+    final radiojson = await rootBundle.loadString("assets/radio.json");
+    radios = MyRadioList.fromJson(radiojson).radios;
+    setState(() {});
+  }
+
   Widget build(BuildContext context) {
-    return Material(
-      color: AIColors.primaryColor1,
+    return Scaffold(
+      drawer: Drawer(),
+      body: Stack(
+        children: [
+          VxAnimatedBox()
+              .size(context.screenWidth, context.screenHeight)
+              .withGradient(
+                LinearGradient(colors: [
+                  AIColors.primaryColor1,
+                  AIColors.primaryColor2,
+                ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+              )
+              .make(),
+          AppBar(
+            title: "A I D I O".text.xl4.size(17).bold.white.make().shimmer(
+                primaryColor: Vx.purple500, secondaryColor: Colors.white),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            centerTitle: true,
+          ).h(100.0).p32(),
+          VxSwiper.builder(
+            itemCount: radios.length,
+            aspectRatio: 1.0,
+            enlargeCenterPage: true,
+            itemBuilder: (context, index) {
+              final rad = radios[index];
+
+              return VxBox(
+                      child: ZStack(
+                [
+                  Positioned(
+                    top: 0.0,
+                    right: 0.0,
+                    child: VxBox(
+                      child: rad.category.text.uppercase.white.make().px16(),
+                    )
+                        .height(40)
+                        .black
+                        .alignCenter
+                        .withRounded(value: 10.0)
+                        .make(),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: VStack(
+                      [
+                        rad.name.text.xl3.white.bold.make(),
+                        5.heightBox,
+                        rad.tagline.text.sm.white.semiBold.make()
+                      ],
+                      crossAlignment: CrossAxisAlignment.center,
+                    ),
+                  ),
+                  Align(
+                      alignment: Alignment.center,
+                      child: [
+                        Icon(
+                          CupertinoIcons.play_circle,
+                          color: Colors.white,
+                        ),
+                        10.heightBox,
+                        "Double tap to play".text.gray300.make(),
+                      ].vStack())
+                ],
+              ))
+                  .clip(Clip.antiAlias)
+                  .bgImage(DecorationImage(
+                      image: NetworkImage(rad.image),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(0.3), BlendMode.darken)))
+                  .withRounded(value: 60.0)
+                  .border(color: Colors.black, width: 5.0)
+                  .make()
+                  .onInkDoubleTap(() {})
+                  .p16();
+            },
+          ).centered(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child:
+                Icon(CupertinoIcons.stop_circle, color: Colors.white, size: 50),
+          ).pOnly(bottom: context.percentHeight * 12)
+        ],
+        fit: StackFit.expand,
+      ),
     );
   }
 }
